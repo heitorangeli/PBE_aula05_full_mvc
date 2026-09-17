32dcb342b02852280edbd785e1dd17145e521b0d
heitorangeli <heitor21dori@gmail.com>
2026-09-17 08:35:39 -0300

    a

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const cors = require("cors")
+
+const rotaInicial = (req, res) => {
+    res.json("Back-end respondendo")
+}
+
+//Configurações do servidor
+const app = express()
+app.use(cors("http://localhost:5500"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+const porta = 3000
+
+//Rotas
+app.get('/', rotaInicial)
+
+app.listen(porta, () => {
+    console.log(`Servidor respondendo em: http://localhost:${porta}`)
+})
